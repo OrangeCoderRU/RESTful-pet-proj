11c138ff338216bf5373325c44fa7d886889e58c
OrangeCoderRU <max.sablin@mail.ru>
2021-03-17 21:48:53 +0300

    initialize project

---
+++ src/main/kotlin/com/example/RESTfulPet/ResTfulPetApplication.kt
@@ -0,0 +1,11 @@
+package com.example.RESTfulPet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ResTfulPetApplication
+
+fun main(args: Array<String>) {
+	runApplication<ResTfulPetApplication>(*args)
+}
